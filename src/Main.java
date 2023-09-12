029ed05685d5e57fcab776ba0d4db5ef825db7b6
Serhat Acar <sserhatacarr@gmail.com>
2023-09-12 11:18:35 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,10 @@ public class Main {
         // Variable defined, created Scanner object and injected to defined variable.
 
         if (year % 4 == 0) {
-            System.out.println("This year is a leap year.");
+            System.out.println( year + " is a leap year.");
         }
         else {
-            System.out.println("This year is not a leap year.");
+            System.out.println(year + " is not a leap year.");
         }
         // The leap year is printed on the screen.
         // According to the remainder of the entered year's division by 4.
