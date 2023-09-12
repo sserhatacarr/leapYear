27f641943b60a64fb6606b6f342a87a622c0857e
Serhat Acar <sserhatacarr@gmail.com>
2023-09-12 11:13:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Leap Year Calculator
+
+- Gets the year from the user.
+- Calculates the leap year.
+- Prints to the screen.
+
+## Author
+
+Serhat Acar
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int year;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        year = input.nextInt();
+        // Variable defined, created Scanner object and injected to defined variable.
+
+        if (year % 4 == 0) {
+            System.out.println("This year is a leap year.");
+        }
+        else {
+            System.out.println("This year is not a leap year.");
+        }
+        // The leap year is printed on the screen.
+        // According to the remainder of the entered year's division by 4.
+    }
+}
